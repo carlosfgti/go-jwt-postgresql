e63fb877749e83fb49f1e1f3b2c444ef750a000b
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-12 08:36:26 -0300

    feat: create first controller

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/carlosfgti/go-jwt-postgresql/cmd/controllers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/health", healthHandler).Methods("GET")
+	router.HandleFunc("/api/v1/users", controllers.CreateUser).Methods("POST")
 
 	port := os.Getenv("PORT")
 	if port == "" {
